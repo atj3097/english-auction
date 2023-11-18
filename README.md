747aa73b3eb21241d034ae6d73f000aeae2c014f
Adam Jackson <18746720+atj3097@users.noreply.github.com>
2023-11-18 16:10:08 -0600

    adding readme and open zeppelin imports

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
-## Foundry
+## English Auction
 
-**Foundry is a blazing fast, portable and modular toolkit for Ethereum application development written in Rust.**
+A seller calls deposit() to deposit an NFT into a contract along with a deadline and a reserve price. Buyers can bid on that NFT up until the deadline, and the highest bid wins. If the reserve price is not met, the NFT is not sold. Multiple auctions can happen at the same time. Buyers who did not win can withdraw their bid. The winner is not able to withdraw their bid and must complete the trade to buy the NFT. The seller can also end the auction by calling sellerEndAuction() which only works after expiration, and if the reserve is met. The winner will be transferred the NFT and the seller will receive the Ethereum.
 
 Foundry consists of:
 
--- src/EnglishAuctionContract.sol
+++ src/EnglishAuctionContract.sol
@@ -2,11 +2,9 @@
 pragma solidity ^0.8.13;
 // A seller calls deposit() to deposit an NFT into a contract along with a deadline and a reserve price. Buyers can bid on that NFT up until the deadline, and the highest bid wins. If the reserve price is not met, the NFT is not sold. Multiple auctions can happen at the same time. Buyers who did not win can withdraw their bid. The winner is not able to withdraw their bid and must complete the trade to buy the NFT. The seller can also end the auction by calling sellerEndAuction() which only works after expiration, and if the reserve is met. The winner will be transferred the NFT and the seller will receive the Ethereum.
 import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/utils/IERC721Receiver.sol";
 
-contract EnglishAuctionContract {
-
-    uint256 public deadline;
-    uint256 public reservePrice;
+contract EnglishAuctionContract is IERC721Receiver {
 
     struct Bid {
         address bidder;
@@ -21,14 +19,19 @@ contract EnglishAuctionContract {
         Bid highestBid;
         mapping(address => Bid) bids;
     }
+    Auction public auction;
 
-    constructor(uint256 _deadline, uint256 _reservePrice) {
-        deadline = _deadline;
-        reservePrice = _reservePrice;
+
+    mapping (address => uint256) public ownerToNFTId;
+
+    constructor() {
     }
 
-    function depositNFT(uint256 _deadline, uint256 _reservePrice) public {
-
+    function depositNFT(uint256 _deadline, uint256 _reservePrice, address nft, uint256 tokenId) public {
+        require(_deadline > block.timestamp, "Deadline must be in the future");
+        auction = Auction(msg.sender, _deadline, _reservePrice, false, Bid(address(0), 0));
+        IERC721(nft).transferFrom(msg.sender, address(this), tokenId);
+        ownerToAuction[msg.sender] = tokenId;
     }
 
 }
